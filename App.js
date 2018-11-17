23506b0f95a510cea0c0fed3b6fc16350a19dfbc
heawen110 <heawen110@gmail.com>
2018-11-17 23:51:10 +0100

    integrate react-redux 🍺

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { Platform, StatusBar, StyleSheet, View } from 'react-native';
 import { AppLoading, Asset, Font, Icon } from 'expo';
 import AppNavigator from './navigation/AppNavigator';
+import {Provider} from 'react-redux';
+import {store} from "./reducers/store";
 
 export default class App extends React.Component {
   state = {
@@ -19,10 +21,12 @@ export default class App extends React.Component {
       );
     } else {
       return (
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
-          <AppNavigator />
-        </View>
+        <Provider store={store}>
+          <View style={styles.container}>
+            {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+            <AppNavigator />
+          </View>
+        </Provider>
       );
     }
   }
