974cd346ed76d1a2353d4e95fc3833dd6c49702d
heawen110 <heawen110@gmail.com>
2018-11-21 23:42:17 +0100

    add reducer logger

--- reducers/store.js
+++ reducers/store.js
@@ -1,10 +1,20 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { walletReducer } from './wallet';
 import { walletImportReducer } from '../modules/WalletImport/walletImportReducer';
+
+
+const logger = store => next => action => {
+  console.group(action.type)
+  console.info('dispatching', action)
+  let result = next(action)
+  console.log('next state', store.getState())
+  console.groupEnd()
+  return result
+}
 
 const reducers = combineReducers({
   wallet: walletReducer,
   walletImport: walletImportReducer,
 });
 
-export const store = createStore(reducers, {});
+export const store = createStore(reducers, applyMiddleware(logger));
