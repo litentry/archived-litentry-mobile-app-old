1ecd0ff6d388c2c7a2136ceb2d94e3ea0b79513f
heawen110 <heawen110@gmail.com>
2018-12-02 15:09:32 +0100

    add common styles

---
+++ commons/commonStyles.js
@@ -0,0 +1,27 @@
+import {
+  StyleSheet,
+  Dimensions,
+  Platform
+} from 'react-native'
+import AppStyle from './AppStyle'
+
+const { width } = Dimensions.get('window')
+
+const fontWeight = Platform.OS === 'ios' ? { fontWeight: 'bold' } : {}
+
+export const openSansRegular = Platform.OS === 'ios' ? 'OpenSans' : 'OpenSans-Regular'
+
+const styles = StyleSheet.create({
+  line: {
+    height: 1,
+    width: width - 40,
+    marginHorizontal: 20,
+    backgroundColor: AppStyle.borderLinesSetting
+  },
+  fontAddress: {
+    fontFamily: Platform.OS === 'ios' ? 'Courier New' : 'CourierNewBold',
+    ...fontWeight
+  }
+})
+
+export default styles
