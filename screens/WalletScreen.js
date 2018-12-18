f87b728bbd3450ffbbe73fe5bbd997f2ba751af8
heawen110 <heawen110@gmail.com>
2018-12-18 03:55:01 +0100

    finish wallet screen style, add genesis button

--- screens/WalletScreen.js
+++ screens/WalletScreen.js
@@ -1,9 +1,12 @@
 import React from 'react';
-import { Text, View, Button } from 'react-native';
+import { Text, View, StyleSheet,} from 'react-native';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 import { walletAction } from '../actions/walletAction';
 import { screensList } from '../navigation/screensList';
+import { Ionicons } from '@expo/vector-icons'
+import AppStyle from "../commons/AppStyle";
+import GenesisButton from "../components/GenesisButton";
 
 const t = {
   createButtonLabel: 'create Wallet',
@@ -15,21 +18,30 @@ class WalletScreen extends React.Component {
   };
 
   static propTypes = {
-    wallet: PropTypes.object.isRequired,
     testAddEth: PropTypes.func.isRequired,
     navigation: PropTypes.object,
   };
 
+  receiveTransaction = () => {}
+
+  sendTransaction = () => {}
+
   render() {
-    const { wallet } = this.props;
     return (
       <View style={styles.container}>
-        <Text>{'wallet value  ' + wallet.eth}</Text>
-        <Button title="Plus one" onPress={this.props.testAddEth} />
-        <Button
-          title={t.createButtonLabel}
-          onPress={() => this.props.navigation.navigate(screensList.WalletCreate.label)}
-        />
+        <View style={styles.displayContainer}>
+          <View style={styles.iconContainer}>
+            <Ionicons name="logo-yen" size={32} color={AppStyle.walletBackgroundColor}/>
+          </View>
+          <View style={styles.textContainer}>
+            <Text style={styles.balanceText}>Balance</Text>
+            <Text style={styles.amountText}>325.67</Text>
+          </View>
+        </View>
+        <View style={styles.actionsContainer}>
+          <GenesisButton action={this.receiveTransaction} text={'Receive'} style={{marginTop: 50}}/>
+          <GenesisButton action={this.sendTransaction} text={'Send'} disabled/>
+        </View>
       </View>
     );
   }
@@ -48,10 +60,40 @@ export default connect(
   mapDispatchToProps
 )(WalletScreen);
 
-const styles = {
+const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: AppStyle.backgroundColor,
+  },
+  displayContainer: {
+    flex: 3,
+    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: AppStyle.walletBackgroundColor,
+  },
+  actionsContainer: {
+    flex: 7,
+  },
+  iconContainer: {
+    height: 50,
+    width: 50,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 25,
+    backgroundColor: 'white',
+  },
+  textContainer: {
+    margin: 10,
     alignItems: 'center',
   },
-};
+  balanceText: {
+    fontSize: AppStyle.fontMiddle,
+    color: 'white',
+    fontWeight: 'bold'
+  },
+  amountText: {
+    fontSize: AppStyle.fontSmall,
+    color: 'white',
+    fontWeight: 'bold'
+  }
+});
