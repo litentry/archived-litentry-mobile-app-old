7c84fe8b0a9091703231aa25eb305406a2f5bfd1
heawen110 <heawen110@gmail.com>
2018-12-30 21:08:06 +0100

    add errorText for input

--- modules/User/components/InputWithValidation.js
+++ modules/User/components/InputWithValidation.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { StyleSheet, TextInput, View, Platform } from 'react-native';
+import { StyleSheet, TextInput, View, Platform, Text } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 import AppStyle from '../../../commons/AppStyle';
 
@@ -11,18 +11,35 @@ export default class InputWithValidation extends React.Component {
     placeholder: PropTypes.string,
     validator: PropTypes.func,
     isPassword: PropTypes.bool,
+    errorMessage: PropTypes.string,
   };
 
   static defaultProps = {
     validator: () => true,
     placeholder: 'please input',
     isPassword: false,
+    errorMessage: 'Input Error'
   };
 
+  renderIcon(isValidated){
+    const { errorMessage, value} = this.props
+    // if (value !== '')
+    //   return null;
+    if(isValidated){
+      return <View style={styles.iconContainer}>
+        <Ionicons name="ios-checkmark-circle-outline" size={32} color={AppStyle.userCorrect} />
+      </View>
+    } else {
+      return <View style={styles.errorContainer}>
+        <Text style={styles.errorText}>{errorMessage}</Text>
+      </View>
+    }
+  }
+
   render() {
-    const { onChangeText, value, placeholder, validator, isPassword } = this.props;
+    const { onChangeText, value, placeholder, validator, isPassword, errorMessage } = this.props;
     const isValidated = validator();
-    const fontColor = isValidated ? AppStyle.userCorrect : AppStyle.userIncorrect;
+    const fontColor = isValidated ? AppStyle.userCorrect : AppStyle.lightGrey;
 
     return (
       <View style={styles.container}>
@@ -35,11 +52,7 @@ export default class InputWithValidation extends React.Component {
           value={value}
           placeholder={placeholder}
         />
-        {isValidated && value !== '' && (
-          <View style={styles.iconContainer}>
-            <Ionicons name="ios-checkmark-circle-outline" size={32} color={AppStyle.userCorrect} />
-          </View>
-        )}
+        {this.renderIcon(isValidated)}
       </View>
     );
   }
@@ -48,6 +61,8 @@ export default class InputWithValidation extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
     marginHorizontal: 50,
     paddingVertical: 10,
     ...Platform.select({
@@ -64,4 +79,19 @@ const styles = StyleSheet.create({
   iconContainer: {
     flex: 1,
   },
+  errorContainer: {
+    position: 'absolute',
+    alignSelf: 'flex-end',
+    left: 0,
+    right: 0,
+    height: 30,
+    bottom: -30,
+    justifyContent: 'center',
+    alignItems: 'flex-end',
+    // backgroundColor: 'yellow',
+  },
+  errorText: {
+    fontSize: AppStyle.fontMiddleSmall,
+    color: AppStyle.userIncorrect,
+  }
 });
