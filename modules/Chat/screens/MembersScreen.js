7071d830e3cf73c292359b1c72012af2bc995d04
heawen110 <heawen110@gmail.com>
2019-01-07 23:42:26 +0100

    add members screen

---
+++ modules/Chat/screens/MembersScreen.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import PropTypes from 'prop-types';
+import connect from 'react-redux/es/connect/connect';
+import _ from 'lodash';
+import { bindActionCreators } from 'redux';
+import AppStyle from '../../../commons/AppStyle';
+import { screensList } from '../../../navigation/screensList';
+import NavigationHeader from '../../../components/NavigationHeader';
+import MemberList from '../components/MemberList';
+
+class MembersScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => ({
+    headerTitle: <NavigationHeader title={screensList.Wallet.title} />,
+    headerBackTitle: '',
+    headerStyle: {
+      backgroundColor: AppStyle.backgroundColor,
+    },
+  });
+
+  static propTypes = {
+    navigation: PropTypes.object,
+  };
+
+  render() {
+    const { navigation } = this.props;
+    return (
+      <View style={styles.container}>
+        <MemberList list={navigation.getParam('list', [])} />
+      </View>
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  walletAddress: state.walletAddress,
+});
+
+const mapDispatchToProps = _.curry(bindActionCreators)({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(MembersScreen);
+
+const styles = StyleSheet.create({
+  container: {},
+});
