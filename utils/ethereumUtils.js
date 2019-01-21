efca3d8a6b8dec8cbd5a517add4b044e0ca487d0
heawen110 <heawen110@gmail.com>
2019-01-21 23:40:42 +0100

    test ethereum utils

--- utils/ethereumUtils.js
+++ utils/ethereumUtils.js
@@ -1,6 +1,9 @@
-import walletUtils from 'ethereumjs-wallet';
+// const secp256k1 = require('secp256k1')
 
-export const getPublicKey = (privateKey) => {
-  const wallet = walletUtils.fromPrivateKey(privateKey)
-  return wallet.getPublicKeyString()
-}
+
+export const getPublicKey = privateKey => {
+  // const wallet = walletUtils.fromPrivateKey(privateKey)
+  // return wallet.getPublicKeyString()
+  // secp256k1.publicKeyCreate(privateKey, false).slice(1)
+  // return privateKey;
+};
