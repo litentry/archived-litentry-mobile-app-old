0271330405fe224c056c9c7078dc85f8702ac764
heawen110 <heawen110@gmail.com>
2019-01-23 22:46:50 +0100

    refractor import via private screen with inner component

---
+++ modules/WalletImport/components/TextWithQRInput.js
@@ -0,0 +1,182 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Clipboard, Platform, SafeAreaView, StyleSheet, Text, TextInput, TouchableOpacity, View, Dimensions} from 'react-native';
+import AppStyle from '../../../commons/AppStyle';
+import _ from 'lodash';
+import { AntDesign } from '@expo/vector-icons';
+import { withNavigation } from 'react-navigation';
+import {screensList} from "../../../navigation/screensList";
+import {lockScreen} from "../../Unlock/lockScreenUtils";
+import LightButton from "../../../components/LightButton";
+import GenesisButton from "../../../components/GenesisButton";
+import Spinner from "../../../components/Spinner";
+import KeyboardView from "../../../components/KeyboardView";
+import TouchOutSideDismissKeyboard from "../../../components/TouchOutSideDismissKeyboard";
+
+const {width} = Dimensions.get('window');
+
+class TextWithQRInput extends React.Component {
+  static propTypes = {
+    onUnlock: PropTypes.func.isRequired,
+    validate: PropTypes.func.isRequired,
+    nextScreen: PropTypes.string.isRequired,
+    errorText: PropTypes.string.isRequired,
+  };
+
+  static defaultProps = {};
+
+  constructor(props){
+    super(props);
+    this.state = {
+      loading: false,
+      input: '',
+    }
+  }
+
+  onPaste = async () => {
+    const content = await Clipboard.getString();
+    if (content) {
+      this.onChangeInput(content);
+    }
+  };
+
+  clearText = () => {
+    this.onChangeInput('');
+  };
+
+  onChangeInput = input => {
+    this.setState({ input: input })
+  };
+
+  gotoScan = () => {
+    this.props.navigation.navigate(screensList.ScanQRCode.label, {
+      onChangePrivateKey: this.onChangeInput.bind(this)
+    });
+  };
+
+  onConfirm = () => {
+    const { navigation } = this.props;
+    lockScreen(navigation).then(this.onImportSuccess);
+  };
+
+  onImportSuccess = () => {
+    const { unUnlock, nextScreen, navigation} = this.props
+    const { input } = this.state
+    this.setState({loading:true})
+    unUnlock(input);
+    this.setState({loading:false})
+    navigation.navigate(nextScreen);
+  };
+
+  render() {
+    const { validate, errorText } = this.props
+    const { loading, input } = this.state
+    const ClearButton = () => (
+      <View style={styles.clearButton}>
+        <TouchableOpacity onPress={this.clearText}>
+          <AntDesign name="delete" size={AppStyle.fontMiddle} color={AppStyle.lightGrey} />
+        </TouchableOpacity>
+      </View>
+    );
+
+    const PasteButton = () => (
+      <View style={styles.pasteButton}>
+        <TouchableOpacity onPress={this.onPaste}>
+          <View style={{ padding: 15 }}>
+            <Text style={styles.pasteText}>{t.PASTE}</Text>
+          </View>
+        </TouchableOpacity>
+      </View>
+    );
+
+    return (
+      <SafeAreaView style={{ flex: 1 }}>
+        <TouchOutSideDismissKeyboard>
+          <View style={styles.container}>
+            <KeyboardView style={styles.container}>
+              <View style={{ marginTop: 25 }}>
+                <TextInput
+                  underlineColorAndroid="transparent"
+                  keyboardAppearance="dark"
+                  autoCorrect={false}
+                  multiline
+                  style={[styles.textInput]}
+                  onChangeText={this.onChangeInput}
+                  value={input}
+                />
+                {input === '' ? <PasteButton /> : <ClearButton />}
+              </View>
+              {!validate(input) && (
+                <Text style={styles.errorText}>{errorText}</Text>
+              )}
+              <LightButton onPress={this.gotoScan} text={t.SCAN_QR_CODE} />
+            </KeyboardView>
+            <GenesisButton
+              containerStyle={styles.button}
+              action={this.onConfirm}
+              disable={input === ''|| !validate(input)}
+              text={t.DONE}
+            />
+            {loading && <Spinner />}
+          </View>
+        </TouchOutSideDismissKeyboard>
+      </SafeAreaView>
+    );
+  }
+}
+
+export default withNavigation(TextWithQRInput)
+
+const t = {
+  PASTE: 'Paste',
+  SCAN_QR_CODE: 'Scan QR Code',
+  DONE: 'Done',
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    backgroundColor: AppStyle.chatBackGroundColor,
+  },
+  textInput: {
+    height: 182,
+    width: width - 40,
+    backgroundColor: 'white',
+    borderRadius: 14,
+    color: AppStyle.lightGrey,
+    fontFamily: Platform.OS === 'ios' ? 'OpenSans' : 'OpenSans-Regular',
+    fontSize: 18,
+    paddingHorizontal: 27,
+    paddingTop: 50,
+    paddingBottom: 50,
+    textAlignVertical: 'center',
+  },
+  errorText: {
+    fontSize: 14,
+    fontFamily: 'OpenSans-SemiBold',
+    color: AppStyle.errorColor,
+    alignSelf: 'flex-start',
+    marginTop: 10,
+    marginLeft: 20,
+  },
+  pasteText: {
+    color: AppStyle.lightGrey,
+    fontFamily: 'OpenSans-SemiBold',
+    fontSize: 16,
+  },
+  button: {
+    width: '100%',
+    position: 'absolute',
+    bottom: 30,
+  },
+  pasteButton: {
+    position: 'absolute',
+    right: 0,
+  },
+  clearButton: {
+    position: 'absolute',
+    right: 15,
+    top: 15,
+  },
+});
--- modules/WalletImport/screens/ImportViaPrivateScreen.js
+++ modules/WalletImport/screens/ImportViaPrivateScreen.js
@@ -1,145 +1,37 @@
 import React, { Component } from 'react';
-import {
-  View,
-  Text,
-  StyleSheet,
-  Dimensions,
-  SafeAreaView,
-  Platform,
-  TextInput,
-  TouchableOpacity,
-  Clipboard,
-} from 'react-native';
 import PropTypes from 'prop-types';
 import connect from 'react-redux/es/connect/connect';
 import _ from 'lodash';
 import { bindActionCreators } from 'redux';
-import { AntDesign } from '@expo/vector-icons';
-import Spinner from '../../../components/Spinner';
 import Checker from '../../../utils/Checker';
-import AppStyle from '../../../commons/AppStyle';
-import KeyboardView from '../../../components/KeyboardView';
-import TouchOutSideDismissKeyboard from '../../../components/TouchOutSideDismissKeyboard';
 import { screensList } from '../../../navigation/screensList';
 import { walletImportAction } from '../walletImportAction';
-import { lockScreen } from '../../Unlock/lockScreenUtils';
-import GenesisButton from '../../../components/GenesisButton';
 import { loaderAction } from '../../../actions/loaderAction';
 import { getPublicKeyFromPrivateKey } from '../../../utils/ethereumUtils';
 import { dataEntry } from '../../../reducers/loader';
-import LightButton from '../../../components/LightButton';
-const { width } = Dimensions.get('window');
+import TextWithQRInput from "../components/TextWithQRInput";
 
 class ImportViaPrivateScreen extends Component {
   static propTypes = {
     navigation: PropTypes.object,
-    setPrivateKey: PropTypes.func.isRequired,
     saveAppData: PropTypes.func.isRequired,
   };
 
   static navigationOptions = {
   };
 
-  constructor(props){
-    super(props);
-    this.state = {
-      loading: false,
-      privateKey: '',
-    }
-  }
-
-  onPaste = async () => {
-    const content = await Clipboard.getString();
-    if (content) {
-      this.onChangePrivateKey(content);
-    }
-  };
-
-  clearText = () => {
-    this.onChangePrivateKey('');
-  };
-
-  onChangePrivateKey = privateKey => {
-    this.setState({ privateKey })
-  };
-
-  gotoScan = () => {
-    this.props.navigation.navigate(screensList.ScanQRCode.label, {
-      onChangePrivateKey: this.onChangePrivateKey.bind(this)
-    });
-  };
-
-  onConfirm = () => {
-    const { navigation } = this.props;
-    lockScreen(navigation).then(this.onImportSuccess);
-  };
-
-  onImportSuccess = () => {
-    this.setState({loading:true})
-    const { navigation, saveAppData } = this.props;
-    const { privateKey } = this.state
+  onUnlock = (privateKey) => {
+    const {saveAppData} = this.props;
     //TODO now I should get the public key and then save it into loader;s place and save private key into secure store.
     // and then split the default screen into two different screens.
     const publicKey = getPublicKeyFromPrivateKey(privateKey);
     saveAppData({ [dataEntry.publicKey.stateName]: publicKey });
-    this.setState({loading:false})
-    navigation.navigate(screensList.Wallet.label);
-  };
+  }
+
+  validate = (privateKey) => privateKey === '' || !_.isEmpty(Checker.checkPrivateKey(privateKey));
 
   render() {
-    const { loading, privateKey } = this.state
-    const isValidPrivateKey = privateKey !== '' && !_.isEmpty(Checker.checkPrivateKey(privateKey));
-    const ClearButton = () => (
-      <View style={styles.clearButton}>
-        <TouchableOpacity onPress={this.clearText}>
-          <AntDesign name="delete" size={AppStyle.fontMiddle} color={AppStyle.lightGrey} />
-        </TouchableOpacity>
-      </View>
-    );
-
-    const PasteButton = () => (
-      <View style={styles.pasteButton}>
-        <TouchableOpacity onPress={this.onPaste}>
-          <View style={{ padding: 15 }}>
-            <Text style={styles.pasteText}>{t.PASTE}</Text>
-          </View>
-        </TouchableOpacity>
-      </View>
-    );
-
-    return (
-      <SafeAreaView style={{ flex: 1 }}>
-        <TouchOutSideDismissKeyboard>
-          <View style={styles.container}>
-            <KeyboardView style={styles.container}>
-              <View style={{ marginTop: 25 }}>
-                <TextInput
-                  underlineColorAndroid="transparent"
-                  keyboardAppearance="dark"
-                  autoCorrect={false}
-                  multiline
-                  style={[styles.textInput]}
-                  onChangeText={this.onChangePrivateKey}
-                  value={privateKey}
-                />
-                {privateKey === '' ? <PasteButton /> : <ClearButton />}
-              </View>
-              {!isValidPrivateKey && privateKey !== '' && (
-                <Text style={styles.errorText}>{t.INVALID_PRIVATE_KEY}</Text>
-              )}
-              <LightButton onPress={this.gotoScan} text={t.SCAN_QR_CODE} />
-            </KeyboardView>
-            <GenesisButton
-              containerStyle={styles.button}
-              action={this.onConfirm}
-              disable={!isValidPrivateKey}
-              text={t.DONE}
-            />
-            {loading && <Spinner />}
-          </View>
-        </TouchOutSideDismissKeyboard>
-      </SafeAreaView>
-    );
+    return <TextWithQRInput onUnlock={this.onUnlock} validate={this.validate} nextScreen={screensList.Wallet.label} errorText={t.INVALID_PRIVATE_KEY}/>
   }
 }
 
@@ -157,57 +49,7 @@ export default connect(
 )(ImportViaPrivateScreen);
 
 const t = {
-  PASTE: 'Paste',
-  SCAN_QR_CODE: 'Scan QR Code',
-  EXISTED_WALLET: 'Wallet already exists.',
   INVALID_PRIVATE_KEY: 'Invalid Private Key.',
-  DONE: 'Done',
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    backgroundColor: AppStyle.chatBackGroundColor,
-  },
-  textInput: {
-    height: 182,
-    width: width - 40,
-    backgroundColor: 'white',
-    borderRadius: 14,
-    color: AppStyle.lightGrey,
-    fontFamily: Platform.OS === 'ios' ? 'OpenSans' : 'OpenSans-Regular',
-    fontSize: 18,
-    paddingHorizontal: 27,
-    paddingTop: 50,
-    paddingBottom: 50,
-    textAlignVertical: 'center',
-  },
-  errorText: {
-    fontSize: 14,
-    fontFamily: 'OpenSans-SemiBold',
-    color: AppStyle.errorColor,
-    alignSelf: 'flex-start',
-    marginTop: 10,
-    marginLeft: 20,
-  },
-  pasteText: {
-    color: AppStyle.lightGrey,
-    fontFamily: 'OpenSans-SemiBold',
-    fontSize: 16,
-  },
-  button: {
-    width: '100%',
-    position: 'absolute',
-    bottom: 30,
-  },
-  pasteButton: {
-    position: 'absolute',
-    right: 0,
-  },
-  clearButton: {
-    position: 'absolute',
-    right: 15,
-    top: 15,
-  },
-});
+
