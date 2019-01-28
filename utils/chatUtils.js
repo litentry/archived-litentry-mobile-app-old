7b89c365f69bca47f89c03c738f60160e63d692f
heawen110 <heawen110@gmail.com>
2019-01-28 01:05:49 +0100

    fix profileImage setting issue

--- utils/chatUtils.js
+++ utils/chatUtils.js
@@ -24,7 +24,7 @@ export const handleCredentialsRequest = (navigation, params) => {
   store.dispatch(
     loaderAction.clearAppData({
       [dataEntry.userId.stateName]: params.user,
-      [dataEntry.profileName.stateName]: params.desc.public.fn,
+      // [dataEntry.profileName.stateName]: params.desc.public.fn,
       [dataEntry.loginToken.stateName]: params.token,
     })
   );
@@ -53,7 +53,6 @@ export const generatePublicInfo = (profileName, imageData) => {
 export const reformatData = (result, value, key) => {
   if (key === 'photo') {
     const avatar = makeImageUrl(value);
-    store.dispatch(chatAction.setAvatar(avatar));
     return _.set(result, 'avatar', avatar);
   }
   if (key === 'fn') {
@@ -64,12 +63,12 @@ export const reformatData = (result, value, key) => {
 
 export const saveUserData = meTopics => {
   if (meTopics && meTopics.public) {
-    const privateData = meTopics.private;
+    const privateData = meTopics.private || {};
     const userInfo = _.reduce(meTopics.public, reformatData, privateData);
     store.dispatch(chatAction.setUserInfo(userInfo));
     store.dispatch(
       loaderAction.saveAppData({
-        [dataEntry.profileImage.stateName]: userInfo.avatar,
+        [dataEntry.profileImage.stateName]: userInfo.avatar ? userInfo.avatar : dataEntry.profileImage.initValue,
         [dataEntry.profileName.stateName]: userInfo.name,
       })
     );
