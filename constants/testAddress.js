ec224fc7cbec44dac8c80f1a21244c5e48b63644
heawen110 <heawen110@gmail.com>
2019-02-01 01:57:49 +0100

    add build info

--- constants/testAddress.js
+++ constants/testAddress.js
@@ -1,2 +1,2 @@
-export const secret = '8fc43c1919a58f1baf255c8a3ac93e439adbce42816601b80862c584c725e0e6';
+export const secret = '';
 export const address = '0x82C7D5fa4e3D2E58a8298C444903589A0499ec08';
