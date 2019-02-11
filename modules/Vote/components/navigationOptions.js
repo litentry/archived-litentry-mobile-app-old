a9d3ff94a3483969d5ffa5657517aaa6647195a8
heawen110 <heawen110@gmail.com>
2019-02-11 01:29:21 +0100

    fix bugs in amend properties

--- modules/Vote/components/navigationOptions.js
+++ modules/Vote/components/navigationOptions.js
@@ -5,7 +5,7 @@ import HeaderButton from '../../../components/HeaderButton';
 export const generateNavigationOptions = title => ({ navigation }) => ({
   headerTitle: title,
   headerRight: (
-    <HeaderButton title={'Done'} onPress={() => () => navigation.goBack()} color={'white'} />
+    <HeaderButton title={'Done'} onPress={() => navigation.goBack()} color={'white'} />
   ),
   headerTintColor: 'white',
 });
